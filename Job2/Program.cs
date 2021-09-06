2f5e170dc6c8fdfe8dc7fab041697573845d8321
Ranulf Erhamion <olegsuyalko@mail.ru>
2021-09-06 12:44:33 +0300

    Добавьте файлы проекта.

---
+++ Job2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Job2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "23456789ABCDEFGHIJK";
+            char[] ch = str.ToCharArray();
+            string vivod = "";
+            Console.WriteLine("Введите число");
+            int des = Convert.ToInt32(Console.ReadLine());
+            if (des > 18)
+            {
+                for (int i = 0; i < des / 18; i++)
+                {
+                    vivod = vivod + "K";
+                }
+                if (des % 18 != 0) vivod = vivod + ch[des % 18 - 1];
+            }
+            else vivod = Convert.ToString(ch[des]);
+            Console.WriteLine(vivod);
+        }
+    }
+}
